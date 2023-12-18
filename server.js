1fba54a5b76073212970d2abf196e65cbf44a660
Ramaneep Singh Bhomrah <88532581+ramandeepbhomrah@users.noreply.github.com>
2023-12-19 01:47:09 +1100

    first commit

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get('/addTwoNumbers/:firstNumber/:secondNumber', function (req, res, next) {
     else { res.json({ result: result, statusCode: 200 }).status(200) }
 })
 
-app.get('/subtractTwoNumbers/:firstNumber/:secondNumber', function (req, res, next) {
+app.get('/divideTwoNumbers/:firstNumber/:secondNumber', function (req, res, next) {
     var firstNumber = parseInt(req.params.firstNumber)
     var secondNumber = parseInt(req.params.secondNumber)
-    var result = firstNumber - secondNumber || null
+    var result = firstNumber / secondNumber || null
     if (result == null) {
         res.json({ result: result, statusCode: 400 }).status(400)
     }
